d260ddaa89be3202cac8e565877fb46c50f24cd1
raisfeld-ori <raisfeldori@gmail.com>
2024-01-01 15:19:02 +0200

    made a beautiful navbar that i totally finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
+    import Navbar from './Navbar.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-
+	<Navbar items={{'test item': '#', 'something': '#'}}/>
 	<main>
 		<slot />
 	</main>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let items = {};
+</script>
+
+<nav class="navbar">
+    {#each Object.entries(items) as [item, url]}
+        <a href={url}>{item}</a>
+    {/each}
+</nav>
+
+<style>
+    .navbar{
+        display: flex;
+        justify-content:left;
+        align-items: center;
+        background-color: #3a3933;
+        height: 3vh;
+        position:fixed;
+        width:100%;
+    }
+    .navbar a {
+        color: white;
+        text-decoration: none;
+        text-align: center;
+        padding: 10px 10px;
+        
+    }
+</style>
