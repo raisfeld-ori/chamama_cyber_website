7e718a3499f0b66d321936d90041cf3d97719b54
ori raisfeld <raisfeldori@gmail.com>
2024-01-02 12:24:31 +0200

    updated nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
     import Navbar from './Navbar.svelte';
 	import './styles.css';
+	import logo from '$lib/images/chamama.png';
 </script>
 
 <div class="app">
-	<Navbar items={{'test item': '#', 'something': '#'}}/>
+	<img id="new_background" src={logo} style="display: none;" alt="">
+	<div class="fadeIn" style="line-height: 1svh;">
+	<a href="https://www.chamama.org" target="_blank" class="hamama-phone"><img src={logo} alt="chamama"></a>
+	<div class="box" id="colorBox">מרחב להתפתחות אישית ויזמות מקיימת</div>
+	<h1 id="head" class="fadeIn">!ברוכים הבאים</h1>
+	<p id="head" class="fadeIn">זה הדף למגמת מחשבים וללמידה שבתוכו</p>
+	<Navbar  items={{'a': 'b', 'c': 'b'}}/>
+	</div>
 	<main>
 		<slot />
 	</main>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,28 +1,35 @@
 <script>
+    import { page } from '$app/stores';
     export let items = {};
 </script>
 
 <nav class="navbar">
     {#each Object.entries(items) as [item, url]}
-        <a href={url}>{item}</a>
+        <a href={url} aria-current={page.current}>{item}</a>
     {/each}
 </nav>
 
+
 <style>
-    .navbar{
-        display: flex;
-        justify-content:left;
-        align-items: center;
-        background-color: #3a3933;
-        height: 3vh;
-        position:fixed;
-        width:100%;
-    }
-    .navbar a {
-        color: white;
-        text-decoration: none;
-        text-align: center;
-        padding: 10px 10px;
-        
-    }
+      .navbar {
+          overflow: hidden;
+          background-color: #1c6ee8;
+          border-radius: 10px;
+          width: 50%;
+          height: 2000%;
+          margin: 0 auto;
+      }
+
+      .navbar a {
+          float: left;
+          display: block;
+          color: #f2f2f2;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+      }
+
+      .navbar a:hover {
+          color: var(--background);
+      }
 </style>
